8cfd0560955474a580292a82396187cfff5cc2d1
Marvie Bobita <avie.sunlight@gmail.com>
2023-11-28 23:54:15 +0800

    add verification, and generate code panel. add google-services.json file for the connection of database

---
+++ app/src/main/java/com/example/dictrictconnect/GenerateCode.kt
@@ -0,0 +1,11 @@
+package com.example.dictrictconnect
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class GenerateCode : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_generate_code)
+    }
+}
---
+++ app/src/main/java/com/example/dictrictconnect/Verification.kt
@@ -0,0 +1,11 @@
+package com.example.dictrictconnect
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Verification : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_verification)
+    }
+}
